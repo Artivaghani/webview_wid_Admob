ece8ce948d00b7aea623cc1f2317b0ee54ccd878
vaghaniarti <vaghaniruhi1@gmail.com>
2021-06-09 22:20:14 +0530

    final code

---
+++ app/src/main/java/com/vaccineregistration/Web_View.java
@@ -0,0 +1,30 @@
+package com.vaccineregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.vaccineregistration.R;
+
+public class Web_View extends AppCompatActivity {
+
+    private WebView wv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web__view);
+
+        wv1=(WebView)findViewById(R.id.webview);
+        wv1.setWebViewClient(new MyBrowser());
+
+        String url = getIntent().getStringExtra("url");
+
+        wv1.getSettings().setLoadsImagesAutomatically(true);
+        wv1.getSettings().setJavaScriptEnabled(true);
+        wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv1.loadUrl(url);
+    }
+}
